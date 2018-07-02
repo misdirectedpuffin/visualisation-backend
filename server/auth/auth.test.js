2d9defffe8cf42ffba971681305db9853f64baf4
misdirectedpuffin <misdirectedpuffin@gmail.com>
2018-07-02 15:40:33 +0100

    Update to eslint specs

--- server/auth/auth.test.js
+++ server/auth/auth.test.js
@@ -2,7 +2,7 @@ const request = require('supertest-as-promised');
 const httpStatus = require('http-status');
 const jwt = require('jsonwebtoken');
 const chai = require('chai'); // eslint-disable-line import/newline-after-import
-const expect = chai.expect;
+const expect = chai.expect; // eslint-disable-line prefer-destructuring
 const app = require('../../index');
 const config = require('../../config/config');
 
